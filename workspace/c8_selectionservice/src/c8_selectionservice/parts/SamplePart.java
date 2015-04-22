e481fc41a9d0805a781bf9166e2bdf18201390ff
Martin Bayer <martin.bay016@gmail.com>
2015-04-22 15:09:12 +0200

    Exampel no.8 added - only simple selection service.

---
+++ workspace/c8_selectionservice/src/c8_selectionservice/parts/SamplePart.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * Copyright (c) 2010 - 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <lars.Vogel@gmail.com> - Bug 419770
+ *******************************************************************************/
+package c8_selectionservice.parts;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.e4.ui.model.application.ui.MDirtyable;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import c8_selectionservice.parts.model.Person;
+
+public class SamplePart {
+
+	private Text txtInput;
+	private TableViewer tableViewer;
+
+	@Inject
+	private ESelectionService selectionService;
+
+	@Inject
+	private MDirtyable dirty;
+
+	@PostConstruct
+	public void createComposite(Composite parent) {
+		parent.setLayout(new GridLayout(1, false));
+
+		txtInput = new Text(parent, SWT.BORDER);
+		txtInput.setMessage("Enter person name");
+		txtInput.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dirty.setDirty(true);
+			}
+		});
+		txtInput.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		tableViewer = new TableViewer(parent);
+		tableViewer.setLabelProvider(new LabelProvider() {
+			@Override
+			public String getText(Object element) {
+				if (element instanceof Person) {
+					return ((Person) element).getName();
+				}
+				return "invalid object";
+			}
+		});
+		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		tableViewer
+				.addSelectionChangedListener(new ISelectionChangedListener() {
+
+					@Override
+					public void selectionChanged(SelectionChangedEvent event) {
+						IStructuredSelection selection = (IStructuredSelection) event
+								.getSelection();
+						selectionService.setSelection(selection
+								.getFirstElement());
+					}
+				});
+	}
+
+	@Focus
+	public void setFocus() {
+		tableViewer.getTable().setFocus();
+	}
+
+	@Persist
+	public void save() {
+		tableViewer.add(new Person(txtInput.getText()));
+		txtInput.setText("");
+		dirty.setDirty(false);
+	}
+
+	@Inject
+	public void itemSelected(Shell shell,
+			@Optional @Named(IServiceConstants.ACTIVE_SELECTION) Person person) {
+		if (person != null) {
+			MessageDialog.openInformation(shell, "Item selected", "Name:"
+					+ person.getName());
+		}
+	}
+}
---
+++ workspace/c8_selectionservice/src/c8_selectionservice/parts/model/Person.java
@@ -0,0 +1,20 @@
+package c8_selectionservice.parts.model;
+
+public class Person {
+
+	private String name;
+
+	public Person(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
